99b6d55f7002e91d914e6c0b06237b65df8930e6
liuzehao <1437070389@qq.com>
2021-06-08 10:30:21 +0800

    自定义错误页面

---
+++ src/components/Error.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bgk">
+        <audio src="../../static/audio/Giao.aac" autoplay></audio>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    @import '../css/Error.css';
+</style>
