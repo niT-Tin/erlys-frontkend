175e62ee0525950f2b65fb6fd6f76bfb99eb2a10
liuzehao <1437070389@qq.com>
2021-06-08 10:36:47 +0800

    裁判查看队员信息界面

---
+++ src/components/referee/PlayerInfo.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="table1">
+  <el-table
+      :data="userinfos"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="school"
+        label="学校"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="coach"
+        label="教练"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="guardian"
+        label="监护人"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="rela"
+        label="关系"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="contact"
+        label="联系方式"
+        width="180">
+      </el-table-column>
+    </el-table>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data (){
+
+      return{
+        userinfos:'',
+      }
+    },
+    created(){
+      this.instance1 = axios.create({
+      baseURL: "http://maymyday.top/flexq/api",
+      timeout: 10000,
+    });
+    this.instance1
+      .get("/getallUserinfo")
+      .then((result) => {
+        this.userinfos = result.data;
+        this.total1 = result.data.length;
+        console.log(this.userinfos);
+      })
+      .catch((err) => {
+        console.log("发生错误");
+      });
+    }
+}
+</script>
+
+<style>
+
+
+@import '../../css/PlayInfo.css';
+
+</style>
