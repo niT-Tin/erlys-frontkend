fe2d1246be8b6a704c75c37c0453f55255311a1e
liuzehao <1437070389@qq.com>
2021-06-08 10:33:24 +0800

    管理员主界面

---
+++ src/components/admin/AAdapter.vue
@@ -0,0 +1,69 @@
+ <template>
+  <div class="top1">
+    <div class="rol">
+      <el-carousel :interval="4000"  type="card">
+        <el-carousel-item  class="itemss" v-for="item in pics" :key="item.id">
+          <h3 class="medium"><img class="images" :src="item.idview" style="display: inline-block; width:100%;" alt=""></h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  <div class="bgk">
+    <div class="title-div">
+      <div class="title-line">{{name}} 你好</div>
+      <div class="title-b">
+        <div class="title">{{title1}}<br/>{{address}}</div>
+        <img class="title-img" src="../../../static/u43.png" alt=""> 
+      </div>
+    </div>
+    <div class="navbar">
+      <el-tabs type="border-card">
+        <el-tab-pane  label="队员信息">
+          <PlayerInfo/>
+        </el-tab-pane>
+        <el-tab-pane label="赛程安排" >
+          <Arrangement/>
+        </el-tab-pane>
+        <el-tab-pane label="裁判信息">
+           <RefereeInfo/>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import qs from "qs";
+import Arrangement from './Arrangement'
+import PlayerInfo from './PlayerInfo'
+import RefereeInfo from './RefereeInfo'
+
+export default {
+  components: {Arrangement, PlayerInfo, RefereeInfo},
+    data(){
+      return {
+         pics: [
+        {id:0, idview: '../../../static/headers/1.jpg'},
+        {id:1, idview: '../../../static/headers/2.jpg'},
+        {id:2, idview: '../../../static/headers/3.jpg'},
+        {id:3, idview: '../../../static/headers/4.jpg'},
+      ]
+      ,
+          name: "管理员",
+          title1: "江西 2021年南昌市保龄球大赛",
+          address: "江西南昌市爱运动保龄球馆"
+      }
+    },
+    methods: {
+
+    },
+    mounted: function(){
+
+    }
+}
+</script>
+
+<style>
+    @import '../../css/AAdapter.css';
+</style>
