3b63daafe30ed5eee4d6ce5ea9986a224a38e4fc
liuzehao <1437070389@qq.com>
2021-06-12 22:48:32 +0800

    修改播放音频部分

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,25 +1,29 @@
 <template>
-    <div class="bgk">
-        <!-- <audio src="../../static/audio/Giao.aac" autoplay></audio> -->
-        <img src="../../static/error.jpg" alt="错误页面">
-        <el-button class="btn1" @click="toLogin" type="primary">点我登录</el-button>
-    </div>
+  <div class="bgk">
+    <audio src="../../static/audio/Giao.aac" autoplay></audio>
+    <img class="er" src="../../static/2.jpg" alt="错误页面" />
+    <div class="fnt">没密码还想看数据？？？</div>
+
+    <el-button class="btn1" @click="toLogin" type="primary">点我登录</el-button>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-    data(){
-        
+  data() {},
+  created() {
+    var audio = new Audio("../../static/audio/Giao.aac");
+    audio.play();
+  },
+  methods: {
+    toLogin() {
+      this.$router.push({ query: { ct: "from_err" }, name: "Login" });
     },
-    methods: {
-        toLogin(){
-            this.$router.push({query:{ct:'from_err'}, name: "Login"});
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-    @import '../css/Error.css';
+@import "../css/Error.css";
 </style>
