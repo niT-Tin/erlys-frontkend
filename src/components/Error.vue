3164805cf53b728151bd7eaebb05ef0728c2b6a0
liuzehao <1437070389@qq.com>
2021-06-12 22:36:16 +0800

    错误页面

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="bgk">
-        <audio src="../../static/audio/Giao.aac" autoplay></audio>
-        
+        <!-- <audio src="../../static/audio/Giao.aac" autoplay></audio> -->
+        <img src="../../static/error.jpg" alt="错误页面">
+        <el-button class="btn1" @click="toLogin" type="primary">点我登录</el-button>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    
+    data(){
+        
+    },
+    methods: {
+        toLogin(){
+            this.$router.push({query:{ct:'from_err'}, name: "Login"});
+        }
+    }
 }
 </script>
 
